64b9daaa9b87889351dfb63c943d1c9cb09729c7
kyrian <ckyrian@protonmail.com>
2021-05-11 13:46:34 +0200

    added basic settings + psql db

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,3 +5,16 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Zhen",
+        "USER": "kyrian",
+        "HOST": "127.0.0.1",
+    }
+}
