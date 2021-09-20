66950411a067134527405abfc68838674228f982
kyrian <ckyrian@protonmail.com>
2021-09-20 22:06:41 +0200

    started writing tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,45 @@
+import json
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import Post, Text
+
+User = get_user_model()
+
+
+class PostTest(TestCase):
+    text_count = 5
+
+    def setUp(self):
+        for i in range(self.text_count):
+            user = User.objects.create_user(
+                username=f"test{i}", email=f"test{i}@example.com", password="secret"
+            )
+            text = Text.objects.create(
+                title="title", original_content="content", author=user
+            )
+            Post.objects.create(
+                text=text,
+                description=f"description{i}",
+                language="en",
+                difficulty="beginner",
+            )
+        client = APIClient()
+
+    def test_post_list(self):
+        response = self.client.get(reverse("posts:list"), format="json")
+        content = json.loads(response.content)
+        self.assertEqual(content["count"], self.text_count)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TextTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(self.username, self.email, self.password)
+        text = Text.objects.create(
+            title="title", original_content="content", author=user
+        )
+        client = APIClient()
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import CustomUser
+from posts.models import Text, Post
+from .serializers import UserProfileTextSerializer
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserTest(TestCase):
+    username = "John"
+    password = "S3cr37"
+    email = "john@example.com"
+
+    def setUp(self):
+        user = User.objects.create_user(self.username, self.email, self.password)
+        text = Text.objects.create(
+            title="title", original_content="content", author=user
+        )
+        client = APIClient()
+
+    def test_uncomplete_user_profile(self):
+        """
+        when user first register, they don't set
+        level and learning_language directly
+        """
+        user = User.objects.get(username=self.username)
+        self.assertEqual(user.level, "unset")
+        self.assertEqual(user.learning_language, "unset")
+
+    def test_get_user_texts(self):
+        user = User.objects.get(username="John")
+        serializer = UserProfileTextSerializer(user)
+        response = self.client.get(reverse("users:texts", kwargs={"pk": user.pk}))
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
